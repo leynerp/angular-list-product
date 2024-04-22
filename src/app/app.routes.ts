fe4a5f2e08b9f2b353f4bb78e1b9e3a2399b5ee4
shonder <shonder@gmail.com>
2024-04-22 15:31:58 -0800

    Revert "feat: add header to the page"
    This reverts commit 0ffa7c469ebdbdef5eab1ded6ec11749662793bb.
    
    this change are of other branch

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,35 +1,4 @@
 import { Route } from '@angular/router';
 
 
-export const APP_ROUTES: Route[] = [
-  {
-    path: 'list',
-    loadComponent: () =>
-      import('./components/product-list/product-list.component').then(
-        (mod) => mod.ProductListComponent,
-      ),
-  },
-  {
-    path: '',
-    pathMatch: 'full',
-    loadComponent: () =>
-      import('./components/product-list/product-list.component').then(
-        (mod) => mod.ProductListComponent,
-      ),
-  },
-  {
-    path: 'details/:id',
-    pathMatch: 'full',
-    loadComponent: () =>
-      import('./components/products-details/products-details.component').then(
-        (mod) => mod.ProductsDetailsComponent,
-      ),
-  },
-  {
-    path: '**',
-    loadComponent: () =>
-      import('./components/page-not-found/page-not-found.component').then(
-        (mod) => mod.PageNotFoundComponent,
-      ),
-  },
-];
+export const APP_ROUTES: Route[] = [];
