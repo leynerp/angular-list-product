f05e17485b84e4cbf912d1cc786cf47e8441cd61
shonder <shonder@gmail.com>
2024-04-22 22:08:26 -0800

    refactor:delete unused import

--- src/app/components/products-details/products-details.component.ts
+++ src/app/components/products-details/products-details.component.ts
@@ -1,10 +1,5 @@
 import { CommonModule } from '@angular/common';
-import { ChangeDetectionStrategy, Component, OnInit, inject, signal } from '@angular/core';
-import { ActivatedRoute, ParamMap, Router } from '@angular/router';
-import { switchMap } from 'rxjs';
-import { ProductsService } from 'src/app/service/products.service';
-import { Gender, ProductDetails } from 'src/app/types/types';
-
+import { ChangeDetectionStrategy, Component } from '@angular/core';
 @Component({
   selector: 'app-products-details',
   standalone: true,
